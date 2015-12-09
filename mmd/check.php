17e99417d6825573e3222bc8c2c55a135cb2b838
Edward-L <linzhexi777@gmail.com>
2015-12-09 18:30:44 +0800

    added mmd

---
+++ mmd/check.php
@@ -0,0 +1,20 @@
+<?php
+
+        $connection = new Mongo("mongodb://admin:supperadmin@120.26.93.115:10000");
+
+        $db = $connection -> HCTF;
+	      $collection = $db -> login;
+
+				$name = $_POST["name"];
+				$password = $_POST["password"];
+				$function = "function(){if(this.name == '$name' && this.password == '$password') return true}";
+				$result = $collection->find(array('$where'=>$function));
+				$check = $result->count();
+				echo "<br/>";
+				if ($check>0){
+							echo "开心吗～233333";
+				}
+				else{
+							echo "nonono";
+				    }
+?>
